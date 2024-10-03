5294c2ad29507b0b9161a2d9643951dcc2e2257d
Ivan <vanja221206@gmail.com>
2024-10-03 19:11:36 +0300

    added kG to rotation regulation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/modules/arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/modules/arm.java
@@ -1,13 +1,16 @@
 package org.firstinspires.ftc.teamcode.subsystems.modules;
 
+import com.arcrobotics.ftclib.controller.wpilibcontroller.ArmFeedforward;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class arm {
     /* ------------------ MOTORS ------------------ */
-    DcMotor rotationMotor;
-    DcMotor extensionMotor;
+    public DcMotor rotationMotor;
+    public DcMotor extensionMotor;
+    public AnalogInput rotationBtn;
 
     /* ------------------ AFTER-INIT-CONSTANTS ------------------ */
     int initialExtensionTicks = 0;
@@ -17,23 +20,26 @@ public class arm {
     int EXTENSION_LOW_CHAMBER = 220;
     int EXTENSION_HIGH_CHAMBER = 280;
     int ROTATION_FRONT = 0;
-    int ROTATION_BACK = 430;
+    int ROTATION_BACK = 450;
     int ROTATION_LIFT = 280;
 
     /* ------------------ ON-FLY ------------------ */
-    int targetRotationPos;
+    public int targetRotationPos;
     int targetExtensionPos;
-    extension extensionState;
-    rotation rotationState;
+    public extension extensionState = extension.CLOSED;
+    public rotation rotationState = rotation.FRONT;
+    public double rotationAngle = -90;
+    public double extensionLen = 0.3;
 
     /* ------------------ PID ------------------ */
-    final double ROTATION_P = 0;
+    final double ROTATION_P = 0.0026;
     final double ROTATION_I = 0;
-    final double ROTATION_D = 0;
-    final double ROTATION_STATIC = 0;
-    final double EXTENSION_P = 0;
-    final double EXTENSION_I = 0;
-    final double EXTENSION_D = 0;
+    final double ROTATION_D = 0.02;
+    final double BASKET_ROTATION_P = 0.004;
+    final double BASKET_ROTATION_D = 0.06;
+    final double EXTENSION_P = 0.02;
+    final double EXTENSION_I = 0.0003;
+    final double EXTENSION_D = 0.002;
     final double EXTENSION_STATIC = 0;
     int oldExtensionError = 0;
     int oldRotationError = 0;
@@ -53,19 +59,24 @@ public class arm {
         FRONT,
         BACK,
         LIFT,
+        RESET,
         MANUAL
     }
     public arm(HardwareMap HM){
         rotationMotor = HM.get(DcMotor.class, "armRotationMotor");
         rotationMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rotationMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rotationMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         extensionMotor = HM.get(DcMotor.class, "armExtensionMotor");
         extensionMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        extensionMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        extensionMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        extensionMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         extensionMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        rotationBtn = HM.get(AnalogInput.class, "rotationBtn");
+        /* prob dont need it */
         initialExtensionTicks = extensionMotor.getCurrentPosition();
         initialRotationTicks = rotationMotor.getCurrentPosition();
 
@@ -82,8 +93,17 @@ public class arm {
     public void update(){
         if (!extensionState.equals(extension.MANUAL))
             setExtension(extensionState);
-        if (!rotationState.equals(rotation.MANUAL))
+        if (!rotationState.equals(rotation.MANUAL) && !rotationState.equals(rotation.RESET))
             setRotation(rotationState);
+        rotationAngle = getRotationAngle();
+        extensionLen = getExtensionLength();
+        if (rotationBtn.getVoltage() < 0.4 && rotationState == rotation.FRONT) /* pressed */ {
+            //TODO: separate function reset
+            rotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            this.rotationState = rotation.RESET;
+            rotationMotor.setPower(0);
+        }
     }
 
     private int extensionPosToTicks(extension target)
@@ -114,8 +134,9 @@ public class arm {
                 return ROTATION_LIFT;
             case FRONT:
                 return ROTATION_FRONT;
+            default:
+                return -1;
         }
-        return -1;
     }
 
     private void setExtensionMotorPower(double power){
@@ -138,12 +159,16 @@ public class arm {
 
 
         oldExtensionError = error;
+
         return (error * EXTENSION_P + delta * EXTENSION_D + extensionSum * EXTENSION_I);
     }
 
-    private double pidCalculateRotationPower(int targetPos){
+    public double pidCalculateRotationPower(int targetPos){
         int error = targetPos - rotationMotor.getCurrentPosition();
         int delta = error - oldRotationError;
+        double kp, kd;
+        //TODO: reorganise kG to be constant defined up in file
+        double kG = 0.03;
 
         if (Math.abs(error) < 40)
             rotationSum += error;
@@ -151,9 +176,30 @@ public class arm {
         if (error * oldRotationError <= 0)
             rotationSum = 0;
 
+        /*
+        switch (rotationState){
+            case LIFT:
+                switch (extensionState){
+                    case MANUAL:
+                        kp = BASKET_ROTATION_P;
+                        kd = BASKET_ROTATION_D;
+                        break;
+                    default:
+                        kp = ROTATION_P;
+                        kd = ROTATION_D;
+                }
+                break;
+            default:
+                kp = ROTATION_P;
+                kd = ROTATION_D;
+        }
+        */
+
+        kp = ROTATION_P;
+        kd = ROTATION_D;
 
         oldRotationError = error;
-        return (error * ROTATION_P + delta * ROTATION_D + rotationSum * ROTATION_I);
+        return (error * kp + delta * kd + kG * Math.sin(Math.toRadians(rotationAngle)) * extensionLen);
     }
     public void setExtension(extension target)
     {
@@ -164,8 +210,16 @@ public class arm {
 
     public void setRotation(rotation target)
     {
-        this.targetRotationPos = rotationPosToTicks(target);
-        this.rotationState = target;
+        if (rotationState == rotation.RESET)
+        {
+            rotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
+        if (extensionState == extension.CLOSED) {
+            this.targetRotationPos = rotationPosToTicks(target);
+            this.rotationState = target;
+        }
         this.setRotationMotorPower(pidCalculateRotationPower(targetRotationPos));
     }
 
@@ -177,5 +231,24 @@ public class arm {
     public void manuallyRotate(double speed){
         this.rotationState = rotation.MANUAL;
         setRotationMotorPower(speed);
+    }
+
+    public void stop(){
+        this.extensionMotor.setPower(0);
+        this.rotationMotor.setPower(0);
+    }
+
+    public double getRotationAngle(){
+        double k = 18 / 45.0;
+        double b = -90;
+
+        return k * rotationMotor.getCurrentPosition() + b;
+    }
+
+    public double getExtensionLength(){
+        double k = 0.7 / 340;
+        double b = 0.3;
+
+        return k * extensionMotor.getCurrentPosition() + b;
     }
 }
